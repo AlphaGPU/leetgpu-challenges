4160bfb020518fab2a88ac730d2955cd180a6655
Kunal Mansukhani <kmmansukhani@gmail.com>
2025-08-10 00:54:18 -0700

    removed comment

--- challenges/medium/36_radix_sort/starter/starter.triton.py
+++ challenges/medium/36_radix_sort/starter/starter.triton.py
@@ -1,4 +1,3 @@
-# The use of PyTorch in Triton programs is not allowed for the purposes of fair benchmarking.
 import triton
 import triton.language as tl
 
@@ -11,4 +10,4 @@ def radix_sort_kernel(
 
 # input_ptr, output_ptr are raw device pointers
 def solve(input_ptr: int, output_ptr: int, N: int):
-    pass 
+    pass
