f49758f7f7d7b435af41eb1138bcda83e2b55009
Ishaan Arya <iarya@umich.edu>
2025-08-26 22:50:05 +0530

    update spec and starter code

--- challenges/medium/47_subarray_sum/starter/starter.cu
+++ challenges/medium/47_subarray_sum/starter/starter.cu
@@ -1,14 +1,6 @@
 #include <cuda_runtime.h>
 
-__global__ void subarray_sum_kernel(const int* input, int* output, int N, int S, int E) {
-
-}
-
 // input, output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const int* input, int* output, int N, int S, int E) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-
-    subarray_sum_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N, S, E);
-    cudaDeviceSynchronize();
+ 
 }
