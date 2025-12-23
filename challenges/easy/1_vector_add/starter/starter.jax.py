6d5ce7a1e7c21e5eee9ebe7371cb97ba49ffe967
Kunal Mansukhani <kmmansukhani@gmail.com>
2025-12-22 21:58:39 -0800

    Fix comment formatting in starter.jax.py (#126)

--- challenges/easy/1_vector_add/starter/starter.jax.py
+++ challenges/easy/1_vector_add/starter/starter.jax.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 
-#A, B are tensors on GPU
+# A, B are tensors on GPU
 @jax.jit
 def solve(A: jax.Array, B: jax.Array, N: int) -> jax.Array:
     # return output tensor directly
