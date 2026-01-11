13f0105fa19e912cebbce897c7bca7c05d727b11
emharsha1812 <harshwardhanfartale.nith@gmail.com>
2026-01-11 20:54:47 +0530

    Style: Fix clang-format violation in starter.cu

--- challenges/medium/64_weight_dequantization/starter/starter.cu
+++ challenges/medium/64_weight_dequantization/starter/starter.cu
@@ -1,6 +1,4 @@
 #include <cuda_runtime.h>
 
 // X, S, Y are device pointers
-extern "C" void solve(float* X, float* S, float* Y, int BLOCK_SIZE) {
-
-}
+extern "C" void solve(float* X, float* S, float* Y, int BLOCK_SIZE) {}
